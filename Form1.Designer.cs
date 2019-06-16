a19bfbd4d8e52b5e9a18810f75d3431334078b58
Sneaky <marceltjeg@gmail.com>
2019-06-16 17:52:23 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Wiskundeteller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Resultatentextveld = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Importeer een tekst bestand";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 186);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(333, 152);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.RichTextBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(124, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 47);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Tellen";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Resultatentextveld
+            // 
+            this.Resultatentextveld.Location = new System.Drawing.Point(362, 56);
+            this.Resultatentextveld.Name = "Resultatentextveld";
+            this.Resultatentextveld.Size = new System.Drawing.Size(359, 252);
+            this.Resultatentextveld.TabIndex = 4;
+            this.Resultatentextveld.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Programma logboek";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(373, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Resultaten";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 349);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Resultatentextveld);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox Resultatentextveld;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+namespace Wiskundeteller
+{
+
+    public partial class Form1 : Form
+    {
+        string textbestand = null;
+        string contents = null;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            Resultatentextveld.Text = "MARCELS WISKUNDE PO PROGRAMMA - 2019 ©";
+            Resultatentextveld.Text = Resultatentextveld.Text + "\n______________________________________________________";
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog theDialog = new OpenFileDialog();
+            theDialog.Title = "Wiskunde PO Tool - Marcel Groeneveld";
+
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                button2.Enabled = true;
+                textbestand = theDialog.FileName;
+                 contents = File.ReadAllText(textbestand);
+                this.richTextBox1.Text = theDialog.FileName + "\nSUCCESSVOL INGELADEN";
+                this.richTextBox1.Text = this.richTextBox1.Text + "\n__________________\nKLAAR OM TE STARTEN\n__________________";
+
+
+
+            }
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RichTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            // Dit wordt uitgevoerd wanneer op button2 wordt geklikt; er is al gevraagt welk tekst bestand te lezen; dat is variabele textbestan
+            // zet de inhoud van textbestand in het geheugen
+            string[] lines = File.ReadAllLines(textbestand);
+         
+            // mylines zijn alle resultaten. Deze tellen we op en koppelen we aan de naam
+            var mylines = lines.GroupBy(g => g).Select(s => new { Name = s, Count = s.Count() });
+
+            //foreach betekent  Voor elke xyz.... dus voor elk getelde regel zet in het textfeld hoe vaak line.name (dus de naam) is geteld (line.count)
+            foreach (var line in mylines)
+            {
+                // We moeten rekening houden met dat een artiest minimaal 16 keer voor komt
+                // we hebben al geteld hoe vaak een artiest voorkomt hierboven dus we kunnen simpel stelen: ALS Line.Count (aantal dus) MEER of gelijk is aan 16; dan pas doen wij iets
+                // >= betekent gelijk of groter dan en tussen haakjes is dan wat er wordt uitgevoerd
+                if (line.Count >= 16)
+                {
+                    Resultatentextveld.Text = Resultatentextveld.Text + "\n" + line.Name.Key + " | " + line.Count;
+                }
+                else
+                { } // Else is wanneer de line.count dus niet gelijk is of minder is dan 16 maar we hoeven met die waardes niets te doen dus laten we dit leeg                 
+                                           
+            }
+            Resultatentextveld.Text = Resultatentextveld.Text + "\n -----------------------------------------";
+            Resultatentextveld.Text = Resultatentextveld.Text + "\n KLAAR MET TELLEN.";
+
+        }
+        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
